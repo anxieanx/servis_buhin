1a38a049b97edb810d5c1d9c9e20425dd36930e4
Ivona Grgić <ivonagrgic96@gmail.com>
2019-03-07 17:20:11 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Servis Buhin</title>
+	<meta name="og:locale" content="hr_HR" />
+	<meta name="og:type" content="website" />
+	<meta name="og:title" content="Servis Buhin" />
+	<meta name="og:description" content="Servis Buhin bavi se servisiranjem i montažom plinskih trošila. Ovlašteni servisni partner za proizvodni program Vaillant i Junker Bosch." />
+	<!-- add url <meta name="og:url" content="" />-->
+	<meta name="og:site_name" content="Servis Buhin" />
+	<meta name="og:image" content="img/logo-baum.png" />
+    <meta name="article:tag" content="Servis Buhin, Osijek, plin, bojler, Vaillant, Bosch, popravak bojlera" />
+    
+    <!-- Share -->
+    <meta property="og:url" content="" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Servis Buhin - servisiranje i popravak plinskih trošila" />
+    <meta property="og:description" content="Servis Buhin bavi se servisiranjem i montažom plinskih trošila. Ovlašteni servisni partner za proizvodni program Vaillant i Junker Bosch. U obrtu trenutno ima četiri zaposlena djelatnika, a svaki od njih je prošao školovanje u predstavništvu tvrtki Vaillant i Junkers u Zagrebu, kao i u sjedištima tvrtki u Njemačkoj." />
+    <meta property="og:image" content="" />
+
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	<link rel="shortcut icon" type="image/png" href="img/logo-baum.png"/>
+	<link rel="stylesheet" href="scss/vendors/owl_carousel.css">
+	<link rel="stylesheet" href="scss/vendors/owl_theme.css">
+	<link rel="stylesheet" href="scss/main.css">
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA==" crossorigin=""/>
+<body>
+
+<?php
+
+
+
+
+
+
+?>
+
+<script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
+	  integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
+      crossorigin="">
+</script> 
+<script src="js/map.js"></script>
+</body>
+</html>
