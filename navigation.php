663f1145c850e0c20c430531fe38917c771135ab
anxieanx <anxie@windowslive.com>
2019-08-27 19:44:44 +0200

    services page responsive

--- navigation.php
+++ navigation.php
@@ -6,9 +6,9 @@
 		<ul class="nav__link--wrap">
 			<li><a href="index.php" class="nav__logo"><span>Servis</span> Buhin</a></li>
 			<li><a href="ponuda.php" class="nav__link">Ponuda</a></li>
-			<li><a href="about.php" class="nav__link">O nama</a></li>
-			<li><a href="gallery.php" class="nav__link">Galerija</a></li>
-			<li><a href="contact.php" class="nav__link">Kontakt</a></li>
+			<li><a href="o-nama.php" class="nav__link">O nama</a></li>
+			<li><a href="galerija.php" class="nav__link">Galerija</a></li>
+			<li><a href="kontakt.php" class="nav__link">Kontakt</a></li>
 		</ul>
 	</div>
 </nav>
@@ -17,9 +17,9 @@
 	<a href="index.php" class="nav__logo"><span>Servis</span> Buhin</a>
 
 	<ul class="nav__link--wrap">
-		<li><a href="offer.php" class="nav__link">Ponuda</a></li>
-		<li><a href="about.php" class="nav__link">O nama</a></li>
-		<li><a href="gallery.php" class="nav__link">Galerija</a></li>
-		<li><a href="contact.php" class="nav__link">Kontakt</a></li>
+		<li><a href="ponuda.php" class="nav__link">Ponuda</a></li>
+		<li><a href="o-nama.php" class="nav__link">O nama</a></li>
+		<li><a href="galerija.php" class="nav__link">Galerija</a></li>
+		<li><a href="kontakt.php" class="nav__link">Kontakt</a></li>
 	</ul>
 </nav>
--- ponuda.php
+++ ponuda.php
@@ -24,7 +24,6 @@
 	<link rel="shortcut icon" type="image/png" href="img/logo-baum.png"/>
 	<link rel="stylesheet" href="css/scss/main.css">
 	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA==" crossorigin=""/>
-	<link rel="stylesheet" href="css/animate.css">
 
 <body>
 	<main class="main grid grid--12">
@@ -32,36 +31,48 @@
 			include 'navigation.php';
 		?>
 
-		<header class="header grid--12">
-			<div class="header__offer">
-				<h2 class="animated fadeInUp">PONUDA</h2>
+		<header class="header header--services grid--12">
+			<section class="services">
+				<h2 class="header__subtitle">Ponuda</h2>
 
-				<div class="services">
-					<div class="service__item animated fadeInUp">
-						<h4>Servis</h4>
-							<p>Servisiranje bojlera obavlja se jednom godišnje čime se dobiva pouzdan, siguran i ekonomičan rad uređaja.</p>
+				<div class="service__item--wrap">
+					<div class="service__item">
+						<div class="service__info">
+							<img src="img/icons/maintenance.png" class="service__img" alt="Servis" />
+							<h3>Servis</h3>
+						</div>
+						<p class="service__desc">Servisiranje bojlera obavlja se jednom godišnje čime se dobiva pouzdan, siguran i ekonomičan rad uređaja.</p>
 					</div>
-					<div class="service__item animated fadeInUp">
-						<h4>Popravak i montaža</h4>
-							<p>Popravak plinskih uređaja, montaža instalacije vode, plina, grijanja i plinskih uređaja.</p>
+
+					<div class="service__item">
+						<div class="service__info">
+							<img src="img/icons/repair.png" class="service__img" alt="Popravak i montaza" />
+							<h3>Popravak i montaža</h3>
+						</div>
+						<p class="service__desc">Popravak plinskih uređaja, montaža instalacije vode, plina, grijanja i plinskih uređaja.</p>
 					</div>
-					<div class="service__item animated fadeInUp">	
-						<h4>Jamstvo</h4>
-							<p>Jamstvo na originalne rezervne dijelove ugrađene u našem ovlaštenom servisu iznosi 6 mjeseci.</p>
+
+					<div class="service__item">
+						<div class="service__info">
+							<img src="img/icons/guarantee.png" class="service__img" alt="Jamstvo" />
+							<h3>Jamstvo</h3>
+						</div>
+						<p class="service__desc">Jamstvo na originalne rezervne dijelove ugrađene u našem ovlaštenom servisu iznosi 6 mjeseci.</p>
 					</div>
-					<div class="service__item animated fadeInUp">
-						<h4>Proljetna akcija</h4>
-							<p>Proljetno servisiranje plinskih i kombi bojlera tijekom svibnja i lipnja po povoljnijim cijenama.</p>
+
+					<div class="service__item">
+						<div class="service__info">
+							<img src="img/icons/discount.png" class="service__img" alt="Proljetna akcija" />
+							<h3>Proljetna akcija</h3>
+						</div>
+						<p class="service__desc">Proljetno servisiranje plinskih i kombi bojlera tijekom svibnja i lipnja po povoljnijim cijenama.</p>
 					</div>
 				</div>
 
+				<a href="#" class="btn btn--primary">Kontaktirajte nas</a>	
+			</section>
 
-				<a href="#" class="btn btn--primary animated fadeInUp">Kontaktirajte nas</a>
-			</div>
-
-			<div class="header__img--wrap">
-				
-			</div>		
+			<div class="header__img--wrap"></div>
 		</header>
 		
 		<?php 
@@ -73,7 +84,5 @@
 	</script> 
 	<script src="js/map.js"></script>
 	<script src="js/burger.js"></script>
-	<script src="js/wow.min.js"></script>
-    <script>new WOW().init();</script>
 </body>
 </html>
