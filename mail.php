6341a2e0f33213ee55ff221d89362f7f4f3e188e
Marija <marijabuhin96@gmail.com>
2019-08-28 20:28:41 +0200

    update mail.php

--- mail.php
+++ mail.php
@@ -2,10 +2,10 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "ivonagrgic96@gmail.com";
+$recipient = "servis.buhin@gmail.com";
 $subject = "[SERVIS BUHIN KONTAKT FORMA]";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Zahvaljujemo na Vašem upitu! "."<br>"."Na Vaš upit odgovorit ćemo u najkraćem mogućem roku.";
 ?>
-<meta http-equiv="refresh" content="10; URL=http://it.ffos.hr/nastava20152016/igrgic/servis_buhin/contact.php">
+<meta http-equiv="refresh" content="10; URL=http://it.ffos.hr/nastava20152016/mbuhin/servis_buhin/contact.php">
